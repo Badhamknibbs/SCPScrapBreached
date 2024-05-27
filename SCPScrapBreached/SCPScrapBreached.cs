de4295d8d2ab87c71058222f4d57d94714f5a174
Badhamknibbs <aydenman@hotmail.com>
2024-05-27 16:42:38 +1000

    Add project files.

---
+++ SCPScrapBreached/SCPScrapBreached.cs
@@ -0,0 +1,82 @@
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using LethalLevelLoader;
+using LobbyCompatibility.Attributes;
+using LobbyCompatibility.Enums;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace SCPScrapBreached
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("BMX.LobbyCompatibility", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency(LethalLevelLoader.Plugin.ModGUID, BepInDependency.DependencyFlags.HardDependency)]
+    [LobbyCompatibility(CompatibilityLevel.Everyone, VersionStrictness.Patch)]
+    public class SCPScrapBreached : BaseUnityPlugin
+    {
+        public static SCPScrapBreached Instance { get; private set; } = null!;
+        internal new static ManualLogSource Logger { get; private set; } = null!;
+        internal static Harmony? Harmony { get; set; }
+
+        public static AssetBundle? SCPScrapAssets = null;
+
+        private void Awake() {
+            Logger = base.Logger;
+            Instance = this;
+
+            NetcodePatcher();
+            Patch();
+
+            string AssemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            SCPScrapAssets = AssetBundle.LoadFromFile(Path.Combine(AssemblyLocation, "scpscrap"));
+            if (SCPScrapAssets == null) {
+                Logger.LogError("Failed to load SCP scrap assets.");
+                return;
+            }
+
+            ExtendedMod? ExtendedMod = SCPScrapAssets.LoadAsset<ExtendedMod>("SCPScrap");
+            if (ExtendedMod == null) {
+                Logger.LogError("Failed to load extended mod from bundle.");
+                return;
+            }
+
+            PatchedContent.RegisterExtendedMod(ExtendedMod);
+
+            Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID} v{MyPluginInfo.PLUGIN_VERSION} has loaded!");
+        }
+
+        internal static void Patch() {
+            Harmony ??= new Harmony(MyPluginInfo.PLUGIN_GUID);
+
+            Logger.LogDebug("Patching...");
+
+            Harmony.PatchAll();
+
+            Logger.LogDebug("Finished patching!");
+        }
+
+        internal static void Unpatch() {
+            Logger.LogDebug("Unpatching...");
+
+            Harmony?.UnpatchSelf();
+
+            Logger.LogDebug("Finished unpatching!");
+        }
+
+        private void NetcodePatcher() {
+            var types = Assembly.GetExecutingAssembly().GetTypes();
+            foreach (var type in types) {
+                var methods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+                foreach (var method in methods) {
+                    var attributes = method.GetCustomAttributes(typeof(RuntimeInitializeOnLoadMethodAttribute), false);
+                    if (attributes.Length > 0) {
+                        method.Invoke(null, null);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SCPScrapBreached/SCP_2176.cs
@@ -0,0 +1,51 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace SCPScrapBreached
+{
+    public class SCP_2176 : GrabbableObject
+    {
+        public float fLightKillRange = 15f;
+
+        // Cause all nearby lights to flicker off
+        public override void PlayDropSFX() {
+            AudioSource component = gameObject.GetComponent<AudioSource>();
+            component.PlayOneShot(itemProperties.dropSFX);
+            WalkieTalkie.TransmitOneShotAudio(component, itemProperties.dropSFX);
+
+            if (IsOwner) {
+                RoundManager.Instance.PlayAudibleNoise(transform.position, 8f, 0.5f, 0, isInElevator && StartOfRound.Instance.hangarDoorsClosed, 941);
+                KillNearbyLightsServerRpc();
+            }
+            hasHitGround = true;
+        }
+
+        private IEnumerator KillNearbyLightsCoroutine() {
+            Vector3 vDropPosition = transform.position;
+            List<Animator> lNearbvLights = RoundManager.Instance.allPoweredLightsAnimators.FindAll(x => Vector3.Distance(vDropPosition, x.transform.position) < fLightKillRange);
+            foreach (Animator light in lNearbvLights) {
+                light.SetTrigger("Flicker");
+                yield return new WaitForSeconds(0.15f);
+            }
+            float fWaitTime = 0.26f + (0.15f * lNearbvLights.Count); // 0.26 is how long the flicker animation lasts, calculate how long we waited for all of the lights in loop above
+            yield return new WaitForSeconds(fWaitTime);
+            foreach (Animator light in lNearbvLights) light.SetBool("on", false);
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        public void KillNearbyLightsServerRpc() {
+            KillNearbyLightsClientRpc();
+        }
+
+        [ClientRpc]
+        public void KillNearbyLightsClientRpc() {
+            StartCoroutine(KillNearbyLightsCoroutine());
+        }
+    }
+}
